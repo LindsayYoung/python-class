b74805fc9dcd68d6aa68611722da8474aed9ef2d
Lindsay Young <lyoung@sunlightfoundation.com>
2014-07-02 18:07:42 -0400

    fix int()

--- lesson-3/temp.py
+++ lesson-3/temp.py
@@ -1,16 +1,18 @@
-temp = raw_input("What is the tempature? ")
+temp = int(raw_input("What is the tempature? "))
 humidity =  raw_input("is humidity high or low ")
 
 # I want a special message for extreme conditions
-if temp < 100 and humidity == "high" :
+if (temp > 100) and (humidity == "high"):
 	print "Heat advisory due to humidity and high tempature. Use extra caution"
-# Want to print this if it is humid but not if it is hot and humid, because the first line is better for that
-elif temp > 80 and humidity == "high":
+# Want to print this if it is humid. I don't want to print the first if and this statement because that would be repetitive. 
+elif (temp > 80) and (humidity == "high"):
 	print "Heat advisory due to humidity"
-# Want to print this if it is hot but not if it is hot and humid, because the first line is better for that
+# Want to print this if it is hot but not if it is hot and humid. If it is hot and humid we already gave the "extra caution" message.
 elif temp > 100:
 	print "Heat advisory due to tempture"
 # we checked all the conditions for a heat advisory and none of them applied
 else:
 	print "No heat advisory"
 
+# If these were all if statements, then on the very hot humid days we would too many repetitive messages.
+# Another benefit is that our else statments keeps track of three different ways there could be a heat advisory. 
