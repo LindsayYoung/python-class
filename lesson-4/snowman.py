905b33fa72e4886bfe86bbd76cd515866c16fa1b
Lindsay Young <lyoung@sunlightfoundation.com>
2014-07-09 14:08:02 -0400

    Made a cute little snowman

--- lesson-4/snowman.py
+++ lesson-4/snowman.py
@@ -1,1 +1,9 @@
 # Make a word guessing game using functions
+snow_1 = "      _     "
+snow_2 = "    _|=|_   "
+snow_3 = "    ('')    "
+snow_4 = ">--( o  )--<"
+snow_5 = "  ( o    )  "
+
+snow_man = [snow_1, snow_2, snow_3, snow_4, snow_5]
+
